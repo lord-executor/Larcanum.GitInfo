749a98d78c9c28f5a7530ea1195b4f40d115be83
Lukas Angerer <executor.dev@gmail.com>
2024-12-20 15:03:18 +0100

    Source template as embedded resource

--- src/GitInfo/GitInfoDummyGenerator.cs
+++ src/GitInfo/GitInfoDummyGenerator.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections.Immutable;
+﻿using System.Reflection;
 using System.Text;
 
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Larcanum.GitInfo
@@ -13,15 +12,22 @@ namespace Larcanum.GitInfo
         public void Initialize(IncrementalGeneratorInitializationContext context)
         {
             context.RegisterPostInitializationOutput(static postInitializationContext => {
-                postInitializationContext.AddSource("GitInfo.g.cs", SourceText.From("""
-                    using System;
-
-                    public partial class GitInfo
-                    {
-                        public const string Commit = "0000000000000000000000000000000000000000";
-                    }
-                    """, Encoding.UTF8));
+                postInitializationContext.AddSource("GitInfo.g.cs", SourceText.From(GetGitInfoTemplate(), Encoding.UTF8));
             });
+        }
+
+        private static string GetGitInfoTemplate()
+        {
+            using var sourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Larcanum.GitInfo.GitInfo.cs.tpl");
+            if (sourceStream != null)
+            {
+                using var reader = new StreamReader(sourceStream);
+                var source = reader.ReadToEnd();
+                source = source.Replace("$[GitIsDirty]", "false").Replace("$[GitCommit]", "0000000000000000000000000000000000000000");
+                return source;
+            }
+
+            return "Unable to load GitInfo.cs.tpl";
         }
     }
 }
