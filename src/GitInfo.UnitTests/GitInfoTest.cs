6651f9740c4cda7e78ee5d923394b5ada28ca856
Lukas Angerer <executor.dev@gmail.com>
2024-12-20 14:08:31 +0100

    Initial commit with project setup

---
+++ src/GitInfo.UnitTests/GitInfoTest.cs
@@ -0,0 +1,12 @@
+﻿using FluentAssertions;
+
+namespace Larcanum.GitInfo.UnitTests;
+
+public class GitInfoTest
+{
+    [Test]
+    public void Success()
+    {
+        true.Should().BeTrue();
+    }
+}
---
+++ src/GitInfo/GitInfoDummyGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Larcanum.GitInfo
+{
+    public class GitInfoDummyGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
