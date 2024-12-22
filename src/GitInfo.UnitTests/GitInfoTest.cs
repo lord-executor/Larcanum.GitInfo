84e2c4cbc79016792b1417858276510157b9d40b
Lukas Angerer <executor.dev@gmail.com>
2024-12-22 17:39:30 +0100

    More simple unit tests of the generated constants

--- src/GitInfo.UnitTests/GitInfoTest.cs
+++ src/GitInfo.UnitTests/GitInfoTest.cs
@@ -1,13 +1,37 @@
-﻿using FluentAssertions;
+﻿using System.Text.RegularExpressions;
+
+using FluentAssertions;
 
 namespace Larcanum.GitInfo.UnitTests;
 
-public class GitInfoTest
+public partial class GitInfoTest
 {
+    [GeneratedRegex(@"^v\d+\.\d+.\d+(?:-[\dA-Za-z\-\.]+)?$")]
+    private partial Regex VersionExpression { get; }
+
     [Test]
-    public void GeneratedClass_GitInfo_CommitExists()
+    public void GeneratedClass_GitInfo_CommitHashExists()
     {
         GitInfo.CommitHash.Should().HaveLength(40);
+    }
+
+    [Test]
+    public void GeneratedClass_GitInfo_CommitShortHashIsPrefixOfCommitHash()
+    {
+        GitInfo.CommitHash.StartsWith(GitInfo.CommitShortHash).Should().BeTrue();
+    }
+
+    [Test]
+    public void GeneratedClass_GitInfo_CommitDateIsInThePast()
+    {
+        DateTime.TryParse(GitInfo.CommitDate, out var commitDate).Should().BeTrue();
+        commitDate.Should().BeBefore(DateTime.Now);
+    }
+
+    [Test]
+    public void GeneratedClass_GitInfo_TagMatchesProjectVersioning()
+    {
+        GitInfo.Tag.Should().MatchRegex(VersionExpression);
     }
 
     [Test]
