ae8cb8335181e1056108845f9f46ab059b444c2a
Lukas Angerer <executor.dev@gmail.com>
2024-12-22 13:09:08 +0100

    Simple console program to output the generated GitInfo

---
+++ src/GitInfo.Out/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+using System.Text.Json;
+
+var options = new JsonSerializerOptions() { WriteIndented = true};
+
+var gitInfo = typeof(GitInfo)
+    .GetFields(BindingFlags.Public | BindingFlags.Static)
+    .ToDictionary(f => f.Name, f => f.GetValue(null));
+Console.WriteLine(JsonSerializer.Serialize(gitInfo, options));
+
+var assembly = Assembly.GetExecutingAssembly();
+var fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+var version = new Dictionary<string, string>
+{
+    ["Version"] = assembly.GetName()?.Version?.ToString() ?? string.Empty,
+    ["FileVersion"] = fvi.FileVersion ?? string.Empty,
+    ["ProductVersion"] = fvi.ProductVersion ?? string.Empty,
+};
+Console.WriteLine(JsonSerializer.Serialize(version, options));
