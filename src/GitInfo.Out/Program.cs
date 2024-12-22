544c1196b921ae74d0f12e4af9d1f7faf5799761
Lukas Angerer <executor.dev@gmail.com>
2024-12-22 16:55:40 +0100

    Explicit Main implementation

--- src/GitInfo.Out/Program.cs
+++ src/GitInfo.Out/Program.cs
@@ -1,21 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Text.Json;
 
-var options = new JsonSerializerOptions() { WriteIndented = true};
+namespace GitInfo.Out;
 
-var gitInfo = typeof(GitInfo.Out.GitInfo)
-    .GetFields(BindingFlags.Public | BindingFlags.Static)
-    .ToDictionary(f => f.Name, f => f.GetValue(null));
-Console.WriteLine(JsonSerializer.Serialize(gitInfo, options));
-
-var assembly = Assembly.GetExecutingAssembly();
-var fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
-var version = new Dictionary<string, string>
+internal static class Program
 {
-    ["Version"] = assembly.GetName()?.Version?.ToString() ?? string.Empty,
-    ["FileVersion"] = fvi.FileVersion ?? string.Empty,
-    ["ProductVersion"] = fvi.ProductVersion ?? string.Empty,
-};
-Console.WriteLine(JsonSerializer.Serialize(version, options));
+    private static readonly JsonSerializerOptions Options = new JsonSerializerOptions() { WriteIndented = true};
+
+    public static void Main(string[] args)
+    {
+        var gitInfo = typeof(GitInfo)
+            .GetFields(BindingFlags.Public | BindingFlags.Static)
+            .ToDictionary(f => f.Name, f => f.GetValue(null));
+        Console.WriteLine("GitInfo");
+        Console.WriteLine(JsonSerializer.Serialize(gitInfo, Options));
+
+        var assembly = Assembly.GetExecutingAssembly();
+        var fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+        var version = new Dictionary<string, string>
+        {
+            ["Version"] = assembly.GetName()?.Version?.ToString() ?? string.Empty,
+            ["FileVersion"] = fvi.FileVersion ?? string.Empty,
+            ["ProductVersion"] = fvi.ProductVersion ?? string.Empty,
+        };
+        Console.WriteLine("AssemblyInfo");
+        Console.WriteLine(JsonSerializer.Serialize(version, Options));
+    }
+}
